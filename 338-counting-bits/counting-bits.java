a4da8ba974ce3b3dbfddcc6b5fefb464173a911d
Abiruban <rameshsoban_bai27@mepcoeng.ac.in>
2026-02-17 22:04:30 +0530

    Time: 2 ms (96.55%) | Memory: 48.7 MB (50.61%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            int count = 0;
+            int num = i;
+            
+            // Count number of 1's in binary representation
+            while (num > 0) {
+                count += num & 1;  // Check if last bit is 1
+                num = num >> 1;     // Right shift by 1 (divide by 2)
+            }
+            dp[i] = count;
+        }
+        return dp;
+    }
+}
