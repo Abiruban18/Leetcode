1dd74f03e01fcf6677025242061ab805ab09645a
Abiruban <rameshsoban_bai27@mepcoeng.ac.in>
2026-02-23 21:02:40 +0530

    Time: 7 ms (98.12%) | Memory: 65.3 MB (57.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,45 +1,35 @@
 class Solution {
-    public List<List<String>> partition(String s) 
-    {
+    public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         List<String> sub = new ArrayList<>();
-        dfs(0, 0, res, sub, s);
+        dfs(0, res, sub, s);
         return res;
     }
     
-    public void dfs(int start, int end, List<List<String>> res, List<String> sub, String s)
-    {
-        if(start >= s.length())
-        {
+    public void dfs(int start, List<List<String>> res, List<String> sub, String s) {
+        if (start == s.length()) {
             res.add(new ArrayList<>(sub));
             return;
         }
-        
-        for(int i = end; i < s.length(); i++)
-        {
-            String substring = s.substring(start, i + 1);
-            
-            if(isPalindrome(substring))
-            {
+        for (int end = start; end < s.length(); end++) {
+            String substring = s.substring(start, end + 1);
+            if (isPalindrome(substring)) {
                 sub.add(substring);
-                dfs(i + 1, i + 1, res, sub, s);
-                sub.remove(sub.size() - 1);
+                dfs(end + 1, res, sub, s);
+                sub.remove(sub.size() - 1); 
             }
         }
     }
     
-    public boolean isPalindrome(String s1)
-    {
+    public boolean isPalindrome(String s1) {
         int l = 0, r = s1.length() - 1;
-        while(l < r)
-        {
-            if(s1.charAt(l) != s1.charAt(r))
-            {
-                return false;
+        while (l < r) {
+            if (s1.charAt(l) != s1.charAt(r)) {
+                return false; 
             }
             l++;
             r--;
         }
-        return true;
-    }   
+        return true;  
+    }
 }
